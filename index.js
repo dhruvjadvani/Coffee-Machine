244bbb168740e148460753d87eb9498b1484b7be
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-27 02:29:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+
+console.log(`Starting to make a coffee
+Grinding coffee beans
+Boiling water
+Mixing boiled water with crushed coffee beans
+Pouring coffee into the cup
+Pouring some milk into the cup
+Coffee is ready!`);
+
+console.log("Write how many cups of coffee you will need:");
+let userInOne = Number(input());
+let water = 200;
+let milk = 50;
+let coffeeBeans = 15;
+console.log(`for ${userInOne} cups of coffee you will need:
+${water * userInOne} ml of water
+${milk * userInOne} ml of milk
+${coffeeBeans * userInOne} g of coffee beans`);
 
