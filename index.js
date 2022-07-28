3a8fbb892e3fdaafef8b6ef77a69d0526db39452
Dhruv Jadvani <dhruv.jadvani@gmail.com>
2022-07-28 21:45:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,75 +17,75 @@ function print() {
 }
 
 function buy() {
-  let coffeeShop = {
-    "espresso" : 1,
-    "latte" : 2,
-    "cappuccino" : 3
-  };
-  console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-  let userBought = input();
-  if(userBought == coffeeShop['espresso']) {
-    // One espresso requires 250 ml of water and 16 g of coffee beans. It costs $4;
-    if(water < 250) {
-        console.log("Sorry, not enough water!");
+    let coffeeShop = {
+        "espresso" : 1,
+        "latte" : 2,
+        "cappuccino" : 3
+    };
+    console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+    let userBought = input();
+    if(userBought == coffeeShop['espresso']) {
+        // One espresso requires 250 ml of water and 16 g of coffee beans. It costs $4;
+        if(water < 250) {
+            console.log("Sorry, not enough water!");
+            return;
+        } else if(beans < 16) {
+            console.log("Sorry, not enough coffee beans!");
+            return;
+        } else {
+            console.log("I have enough resources, making you a coffee!");
+        }
+        water = water - 250;
+        beans = beans - 16;
+        money = money + 4;
+        cups = cups - 1;
+        
+        
+    } else if(userBought == coffeeShop['latte']) {
+        // One latte requires 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7;
+        if(water < 350) {
+            console.log("Sorry, not enough water!");
+            return;   
+        } else if(milk < 75) {
+            console.log("Sorry, not enough milk!");
+            return;
+        } else if(beans < 20) {
+            console.log("Sorry, not enough coffee beans!");
+            return;
+        } else {
+            console.log("I have enough resources, making you a coffee!");
+        }
+        water = water - 350;
+        milk = milk - 75;
+        beans = beans - 20;
+        money = money + 7;
+        cups = cups - 1;
+        
+        
+    } else if(userBought == coffeeShop['cappuccino']) {
+        // One cappuccino requires 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
+        if(water < 200) {
+            console.log("Sorry, not enough water!");
+            return;
+        } else if(milk < 100) {
+            console.log("Sorry, not enough milk!");
+            return;
+        } else if(beans < 12) {
+            console.log("Sorry, not enough coffee beans!");
+            return;
+        } else {
+            console.log("I have enough resources, making you a coffee!");
+        }
+        water = water - 200;
+        milk = milk - 100;
+        beans = beans - 12;
+        money = money + 6;
+        cups = cups - 1;
+        
+        
+    } else if(userBought == 'back' || userBought == 'remaining') {
         return;
-    } else if(beans < 16) {
-        console.log("Sorry, not enough coffee beans!");
-        return;
-    } else {
-        console.log("I have enough resources, making you a coffee!");
-    }
-    water = water - 250;
-    beans = beans - 16;
-    money = money + 4;
-    cups = cups - 1;
-    
-    
-  } else if(userBought == coffeeShop['latte']) {
-    // One latte requires 350 ml of water, 75 ml of milk, and 20 g of coffee beans. It costs $7;
-    if(water < 350) {
-        console.log("Sorry, not enough water!");
-        return;   
-    } else if(milk < 75) {
-        console.log("Sorry, not enough milk!");
-        return;
-    } else if(beans < 20) {
-        console.log("Sorry, not enough coffee beans!");
-        return;
-    } else {
-        console.log("I have enough resources, making you a coffee!");
-    }
-    water = water - 350;
-    milk = milk - 75;
-    beans = beans - 20;
-    money = money + 7;
-    cups = cups - 1;
-    
-    
-  } else if(userBought == coffeeShop['cappuccino']) {
-    // One cappuccino requires 200 ml of water, 100 ml of milk, and 12 g of coffee beans. It costs $6.
-    if(water < 200) {
-        console.log("Sorry, not enough water!");
-        return;
-    } else if(milk < 100) {
-        console.log("Sorry, not enough milk!");
-        return;
-    } else if(beans < 12) {
-        console.log("Sorry, not enough coffee beans!");
-        return;
-    } else {
-        console.log("I have enough resources, making you a coffee!");
-    }
-    water = water - 200;
-    milk = milk - 100;
-    beans = beans - 12;
-    money = money + 6;
-    cups = cups - 1;
-    
-    
-  } else if(userBought == 'back' || userBought == 'remaining') {
-    return;
-  } 
+    } 
 
 }
 
